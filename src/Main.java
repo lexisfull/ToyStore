e0bdfd6129d706e535786a718af6e8297160751b
aleksey <alexeypanov29@gmail.com>
2023-12-15 23:19:31 +0300

    smoke break

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 import Controller.Controller;
-import Model.FabricToys;
-import Model.IFabricToys;
-import Model.Toys;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
