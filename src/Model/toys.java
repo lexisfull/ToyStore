53893e83355bd4b6dcee8c1676b8114f3ac090ac
aleksey <alexeypanov29@gmail.com>
2023-12-15 09:04:50 +0300

    Add model

---
+++ src/Model/toys.java
@@ -0,0 +1,13 @@
+package Model;
+
+public class toys {
+    private int id;
+    private String name;
+    private  int  weight;
+
+    public toys(int id, String name, int weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+}
